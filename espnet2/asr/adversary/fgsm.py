8af759826e875eecdeb605aa7e01876d6ade8687
Pengcheng Guo <guopengcheng1220@gmail.com>
2021-06-17 15:50:31 +0800

    Support FGSM adv training.

--- espnet2/asr/adversary/fgsm.py
+++ espnet2/asr/adversary/fgsm.py
@@ -4,6 +4,8 @@
 
 """FastGradientSignMethod attack definition."""
 
+import torch
+
 
 class FastGradientSignMethod(object):
     """One step FastGradientSignMethod (Goodfellow et al, 2014)
@@ -13,10 +15,10 @@ class FastGradientSignMethod(object):
 
     def __init__(
         self,
-        adv_begin_epoch=0,
-        adv_prob=1.0,
-        adv_lr=0.1,
-        adv_weight=0.3,
+        adv_begin_epoch: int = 0,
+        adv_prob: float = 1.0,
+        adv_lr: float = 0.1,
+        adv_weight: float = 0.3,
     ):
         """Construct a FastGradientSignMethod object.
 
@@ -35,5 +37,30 @@ class FastGradientSignMethod(object):
         self.adv_lr = adv_lr
         self.adv_weight = adv_weight
 
-    def __call__(self, model):
+    def __call__(
+        self,
+        model: torch.nn.Module,
+        batch: dict,
+        no_sync_dp: bool = True,
+    ):
+        import pdb
+
+        pdb.set_trace()
+        speech = batch["speech"].requires_grad_()  # for adversarial training
+        speech_lengths = batch["speech_lengths"]
+        text = batch["text"]
+        text_lengths = batch["text_lengths"]
+
+        loss, _, _ = model(
+            speech,
+            speech_lengths,
+            text,
+            text_lengths,
+            no_sync_dp=False,
+        )
+
+        grad_speech = torch.autograd.grad(
+            loss, speech, torch.ones_like(loss), retrain_graph=True
+        )
+
         return model
--- espnet2/train/trainer.py
+++ espnet2/train/trainer.py
@@ -13,6 +13,7 @@ from typing import Optional
 from typing import Sequence
 from typing import Tuple
 from typing import Union
+from chainer import training
 
 import humanfriendly
 import numpy as np
@@ -485,77 +486,18 @@ class Trainer:
                 all_steps_are_invalid = False
                 continue
 
-            with autocast(scaler is not None):
-                with reporter.measure_time("forward_time"):
-                    if adversary is None:
-                        retval = model(**batch, no_sync_dp=True)
-                    else:
-                        raise NotImplementedError
-
-                    # Note(kamo):
-                    # Supporting two patterns for the returned value from the model
-                    #   a. dict type
-                    if isinstance(retval, dict):
-                        loss = retval["loss"]
-                        stats = retval["stats"]
-                        weight = retval["weight"]
-                        optim_idx = retval.get("optim_idx")
-                        if optim_idx is not None and not isinstance(optim_idx, int):
-                            if not isinstance(optim_idx, torch.Tensor):
-                                raise RuntimeError(
-                                    "optim_idx must be int or 1dim torch.Tensor, "
-                                    f"but got {type(optim_idx)}"
-                                )
-                            if optim_idx.dim() >= 2:
-                                raise RuntimeError(
-                                    "optim_idx must be int or 1dim torch.Tensor, "
-                                    f"but got {optim_idx.dim()}dim tensor"
-                                )
-                            if optim_idx.dim() == 1:
-                                for v in optim_idx:
-                                    if v != optim_idx[0]:
-                                        raise RuntimeError(
-                                            "optim_idx must be 1dim tensor "
-                                            "having same values for all entries"
-                                        )
-                                optim_idx = optim_idx[0].item()
-                            else:
-                                optim_idx = optim_idx.item()
-
-                    #   b. tuple or list type
-                    else:
-                        loss, stats, weight = retval
-                        optim_idx = None
-
-                stats = {k: v for k, v in stats.items() if v is not None}
-                if ngpu > 1 or distributed:
-                    # Apply weighted averaging for loss and stats
-                    loss = (loss * weight.type(loss.dtype)).sum()
-
-                    # if distributed, this method can also apply all_reduce()
-                    stats, weight = recursive_average(stats, weight, distributed)
-
-                    # Now weight is summation over all workers
-                    loss /= weight
-                if distributed:
-                    # NOTE(kamo): Multiply world_size because DistributedDataParallel
-                    # automatically normalizes the gradient by world_size.
-                    loss *= torch.distributed.get_world_size()
-
-                loss /= accum_grad
-
-            reporter.register(stats, weight)
-
-            with reporter.measure_time("backward_time"):
-                if scaler is not None:
-                    # Scales loss.  Calls backward() on scaled loss
-                    # to create scaled gradients.
-                    # Backward passes under autocast are not recommended.
-                    # Backward ops run in the same dtype autocast chose
-                    # for corresponding forward ops.
-                    scaler.scale(loss).backward()
-                else:
-                    loss.backward()
+            if adversary is None:
+                stats = cls.train_step(
+                    batch,
+                    model,
+                    scaler,
+                    reporter,
+                    options,
+                    distributed_option,
+                )
+            else:
+                stats = adversary.train_step()
+            optim_idx = stats.get("optim_idx")
 
             if iiter % accum_grad == 0:
                 if scaler is not None:
@@ -653,6 +595,89 @@ class Trainer:
                 torch.distributed.all_reduce(iterator_stop, ReduceOp.SUM)
 
         return all_steps_are_invalid
+
+    @classmethod
+    def train_step(
+        cls,
+        batch: dict,
+        model: torch.nn.Module,
+        scaler: Optional[GradScaler],
+        reporter: SubReporter,
+        options: TrainerOptions,
+        distributed_option: DistributedOption,
+    ):
+        with autocast(scaler is not None):
+            with reporter.measure_time("forward_time"):
+                retval = model(**batch, no_sync_dp=True)
+
+                # Note(kamo):
+                # Supporting two patterns for the returned value from the model
+                #   a. dict type
+                if isinstance(retval, dict):
+                    loss = retval["loss"]
+                    stats = retval["stats"]
+                    weight = retval["weight"]
+                    optim_idx = retval.get("optim_idx")
+                    if optim_idx is not None and not isinstance(optim_idx, int):
+                        if not isinstance(optim_idx, torch.Tensor):
+                            raise RuntimeError(
+                                "optim_idx must be int or 1dim torch.Tensor, "
+                                f"but got {type(optim_idx)}"
+                            )
+                        if optim_idx.dim() >= 2:
+                            raise RuntimeError(
+                                "optim_idx must be int or 1dim torch.Tensor, "
+                                f"but got {optim_idx.dim()}dim tensor"
+                            )
+                        if optim_idx.dim() == 1:
+                            for v in optim_idx:
+                                if v != optim_idx[0]:
+                                    raise RuntimeError(
+                                        "optim_idx must be 1dim tensor "
+                                        "having same values for all entries"
+                                    )
+                            optim_idx = optim_idx[0].item()
+                        else:
+                            optim_idx = optim_idx.item()
+
+                #   b. tuple or list type
+                else:
+                    loss, stats, weight = retval
+                    optim_idx = None
+
+            stats = {k: v for k, v in stats.items() if v is not None}
+            if options.ngpu > 1 or distributed_option.distributed:
+                # Apply weighted averaging for loss and stats
+                loss = (loss * weight.type(loss.dtype)).sum()
+
+                # if distributed, this method can also apply all_reduce()
+                stats, weight = recursive_average(
+                    stats, weight, distributed_option.distributed
+                )
+
+                # Now weight is summation over all workers
+                loss /= weight
+            if distributed_option.distributed:
+                # NOTE(kamo): Multiply world_size because DistributedDataParallel
+                # automatically normalizes the gradient by world_size.
+                loss *= torch.distributed.get_world_size()
+
+            loss /= options.accum_grad
+
+        reporter.register(stats, weight)
+
+        with reporter.measure_time("backward_time"):
+            if scaler is not None:
+                # Scales loss.  Calls backward() on scaled loss
+                # to create scaled gradients.
+                # Backward passes under autocast are not recommended.
+                # Backward ops run in the same dtype autocast chose
+                # for corresponding forward ops.
+                scaler.scale(loss).backward()
+            else:
+                loss.backward()
+
+        return stats
 
     @classmethod
     @torch.no_grad()
